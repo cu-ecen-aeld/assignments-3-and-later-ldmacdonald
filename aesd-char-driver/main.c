daee19338715968f6c21eefa869b7043c1913916
Dale MacDonald <lawrence.macdonald@colorado.edu>
2025-04-24 00:58:11 -0700

    Adding ioctl implementation to repository

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -17,21 +17,29 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
+#include <linux/errno.h>
+#include <linux/string.h>
+#include <linux/slab.h>
 #include "aesdchar.h"
+#include "aesd_ioctl.h"
+
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Dale MacDonald"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
 
 int aesd_open(struct inode *inode, struct file *filp)
 {
+    struct aesd_dev *dev;
     PDEBUG("open");
     /**
      * TODO: handle open
      */
+    dev = container_of(inode->i_cdev, struct aesd_dev, cdev);
+    filp->private_data = dev;
     return 0;
 }
 
@@ -48,10 +56,52 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = 0;
+    ssize_t num_of_available_bytes = 0;
+    unsigned long bytes_not_copied;
+    ssize_t offset_within_entry;
+    struct aesd_circular_buffer *buffer = ((struct aesd_dev *)(filp->private_data))->buffer;
+    struct aesd_buffer_entry *this_entry;
+    char *output_buffer = (char *)kzalloc(count * sizeof(char), GFP_KERNEL);
+
+    if (!output_buffer) {
+        printk(KERN_WARNING "Can't reallocate memory for read buffer: %lu bytes\n", count);
+        return -ENOMEM;
+    }
+
     PDEBUG("read %zu bytes with offset %lld",count,*f_pos);
     /**
      * TODO: handle read
      */
+
+    this_entry = aesd_circular_buffer_find_entry_offset_for_fpos(buffer, *f_pos, &offset_within_entry);
+    
+    if (!this_entry) {
+        kfree(output_buffer);
+        return 0;
+    }
+
+    num_of_available_bytes = this_entry->size - offset_within_entry;
+
+    if (num_of_available_bytes > count){
+        strncpy(output_buffer, this_entry->buffptr + offset_within_entry, count);
+        retval = count;
+        *f_pos += count;
+    } else {
+        strncpy(output_buffer, this_entry->buffptr + offset_within_entry, num_of_available_bytes);
+        retval = num_of_available_bytes;
+        *f_pos += num_of_available_bytes;
+    }
+
+    bytes_not_copied = copy_to_user(buf, output_buffer, retval);
+
+    if(bytes_not_copied) {
+        retval -= bytes_not_copied;
+        printk(KERN_WARNING "Could not copy all data to user space: %lu bytes\n", bytes_not_copied);
+        kfree(output_buffer);
+        return retval;
+    }
+
+    kfree(output_buffer);
     return retval;
 }
 
@@ -59,18 +109,168 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
                 loff_t *f_pos)
 {
     ssize_t retval = -ENOMEM;
+    struct aesd_circular_buffer *buffer = ((struct aesd_dev *)(filp->private_data))->buffer;
+    struct aesd_buffer_entry *new_entry = ((struct aesd_dev *)(filp->private_data))->add_entry;
+    char *new_entry_string;
+    const char *contents_of_previous_write;
+    char *temp_new_ptr;
+    char *local_buf;
+    char has_newline = 0;
+    int i = 0;
+
     PDEBUG("write %zu bytes with offset %lld",count,*f_pos);
+
+    if (mutex_lock_interruptible(&(((struct aesd_dev *)(filp->private_data))->lock))) {
+        printk(KERN_WARNING "Interrupted, waiting on mutex lock");
+        return -EINTR;
+    }
+
+    local_buf = (char *)kzalloc(count * sizeof(char), GFP_KERNEL);
+
+    if (new_entry->buffptr == NULL){
+        new_entry_string = (char *)(kzalloc(count * sizeof(char), GFP_KERNEL));
+        if(!new_entry_string){
+            printk(KERN_WARNING "Can't allocate memory for string to write: %lu bytes\n", count);
+            mutex_unlock(&(((struct aesd_dev *)(filp->private_data))->lock));
+            return -ENOMEM;
+        }
+    } else {
+        contents_of_previous_write = new_entry->buffptr;
+        temp_new_ptr = (char *)(kzalloc(new_entry->size + (count * sizeof(char)), GFP_KERNEL));
+
+        if (temp_new_ptr) {
+            memcpy(temp_new_ptr, contents_of_previous_write, new_entry->size + count);
+            kfree(contents_of_previous_write);
+            new_entry_string = temp_new_ptr;
+        } else {
+            printk(KERN_WARNING "Can't reallocate memory for string to write: %lu bytes\n", count);
+            mutex_unlock(&(((struct aesd_dev *)(filp->private_data))->lock));
+            return -ENOMEM;
+        }
+    }
+
+    retval = copy_from_user(local_buf, buf, count);
+    if (reval) {
+        printk(KERN_WARNING "Could not copy all user data passed to write(): %lu bytes not written\n", retval);
+        count -= retval;
+    }
+
+    strncat(new_entry_string, local_buf, count);
+    kfree(local_buf);
+    new_entry->buffptr = new_entry_string;
+    PDEBUG("Working entry size was %zu;", new_entry->size);
+    new_entry->size += count;
+    PDEBUG("now %zu\n", new_entry->size);
+    for (i = 0; i < new_entry->size; i++){
+        if (new_entry->buffptr[i] == '\n'){
+            has_newline = 1;
+        }
+    }
+
+    if (has_newline){
+        if(buffer->full){
+            kfree(buffer->entry[buffer->in_offs].buffptr);
+        }
+        aesd_circular_buffer_add_entry(buffer, new_entry);
+        *f_pos += new_entry->size;
+        new_entry->size = 0;
+        new_entry->buffptr = NULL;
+        mutex_unlock(&(((struct aesd_dev *)(filp->private_data))->lock));
+        return count;
+    } else {
+        PDEBUG("No newline in string to write; wrote %zu byets\n", count);
+        mutex_unlock(&(((struct aesd_dev *)(filp->private_data))->lock));
+        return count;
+    }
     /**
      * TODO: handle write
      */
-    return retval;
+    //return retval;
 }
+
+loff_t aesd_llseek(struct file *filp, loff_t off, int whence) {
+    struct aesd_dev *dev= filp->private_data;
+    struct aesd_circular_buffer *buffer = dev->buffer;
+    loff_t size = 0;
+    int i = 0;
+
+    PDEBUG("llseek %llu, whence %d\n", off, whence);
+    if (mutex_lock_interruptible(&(((struct aesd_dev *)(filp->private_data))->lock))) {
+        printk(KERN_WARNING "Interrupted waiting on mutex lock");
+        return -ERESTARTSYS;
+    }
+
+    for (i = 0; i < AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED; i++) {
+        size += buffer->entry[i].size;
+    }
+
+    mutex_unlock(&(((struct aesd_dev *)(filp->private_data))->lock));
+    return fixed_size_llseek(filp, off, whence, size);
+}
+
+static long aesd_adjust_file_offset(struct file *filp, unsigned int write_cmd, unsigned int write_cmd_offset){
+    struct aesd_dev *dev = filp->private_data;
+    struct aesd_circular_buffer *buffer = dev->buffer;
+    int i;
+    int length_of_writes;
+
+    if(mutex_lock_interruptible(&dev->lock)) {
+        printk(KERN_WARNING "aesd_adjust_file_offset: Interrupted waiting on mutex lock");
+        return -ERESTARTSYS;
+    }
+
+    if (write_cmd >= 10) {
+        mutex_unlock(&dev->lock);
+        printk(KERN_WARNING "aesd_adjust_file_offset: write_cmd value to large");
+        return -EINVAL;
+    }
+
+    if (buffer->entry[write_cmd].size == 0 || buffer->entry[write_cmd].size < write_cmd_offset) {
+        mutex_unlock(&dev->lock);
+        printk(KERN_WARNING "aesd_adjust_file_offset: write_cmd not written or offset too large");
+        return -EINVAL;
+    }
+
+    PDEBUG("aesd_adjust_file_offset cmd %u, offset %u\n", write_cmd, write_cmd_offset);
+
+    if(write_cmd < buffer->out_offs){
+        write_cmd += AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    for (i = write_cmd; i > buffer->out_offs; i--){
+        int cmd_len = buffer->entry[i % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED].size;
+        length_of_writes += cmd_len;
+        PDEBUG("aesd_adjust_file_offset: inspect cmd %u, add length %d, total %d\n", (i % 10), cmd_len, length_of_writes);
+    }
+
+    filp->f_pos = length_of_writes + write_cmd_offset;
+    mutex_unlock(&dev->lock);
+    return 0;
+}
+
+long int aesd_ioctl(struct file *filp, unsigned int cmd, unsigned long arg){
+    switch(cmd) {
+        case AESDCHAR_IOCSEEKTO:
+          struct aesd_seekto seekto;
+          if(copy_from_user(&seekto, (const void __user *)arg, sizeof(seekto)) != 0) {
+            return -EFAULT;
+          } else {
+            return aesd_adjust_file_offset(filp, seekto.write_cmd, seekto.write_cmd_offset);
+          }
+          break;
+        default:
+          return -EINVAL;
+    }
+}
+
 struct file_operations aesd_fops = {
     .owner =    THIS_MODULE,
     .read =     aesd_read,
     .write =    aesd_write,
     .open =     aesd_open,
     .release =  aesd_release,
+    .llseek = aesd_llseek,
+    .unlocked_ioctl = aesd_ioctl,
 };
 
 static int aesd_setup_cdev(struct aesd_dev *dev)
@@ -105,6 +305,21 @@ int aesd_init_module(void)
     /**
      * TODO: initialize the AESD specific portion of the device
      */
+    aesd_device.buffer = (struct aesd_circular_buffer *)(kzalloc(sizeof(struct aesd_circular_buffer), GFP_KERNEL));
+    if(!aesd_device.buffer){
+        printk(KERN_WARNING "Can't allocate memory for circular buffer\n");
+        unregister_chrdev_region(dev, 1);
+        return -ENOMEM:
+    }
+
+    aesd_device.add_entry = (struct aesd_buffer_entry *)(kzalloc(sizeof(struct aesd_buffer_entry), GFP_KERNEL));
+    if(!aesd_device.add_entry){
+        printk(KERN_WARNING "Can't allocate memory for working entry\n");
+        unregister_chrdev_region(dev, 1);
+        return -ENOMEM;
+    }
+
+    mutex_init(&(aesd_device.lock));
 
     result = aesd_setup_cdev(&aesd_device);
 
@@ -117,6 +332,8 @@ int aesd_init_module(void)
 
 void aesd_cleanup_module(void)
 {
+    int i;
+    struct aesd_buffer_entry *entry;
     dev_t devno = MKDEV(aesd_major, aesd_minor);
 
     cdev_del(&aesd_device.cdev);
@@ -124,6 +341,21 @@ void aesd_cleanup_module(void)
     /**
      * TODO: cleanup AESD specific poritions here as necessary
      */
+
+    mutex_destroy(&(aesd_device.lock));
+
+    if(aesd_device.buffer){
+        AESD_CIRCULAR_BUFFER_FOREACH(entry, aesd_device.buffer, i){
+            if(entry->buffptr){
+                kfree(entry->buffptr);
+            }
+        }
+        kfree(aesd_device.buffer);
+    }
+
+    if (aesd_device.add_entry){
+        kfree(aesd_device.add_entry);
+    }
 
     unregister_chrdev_region(devno, 1);
 }
